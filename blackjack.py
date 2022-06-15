31a8ec8eab627381f7bbb1605fe9a5adff75e0f2
wiederin <wiederin@usc.edu>
2022-06-15 12:04:19 +0200

    fixed generate_episode_data (return in wrong place) + fv_mc_pred none check

--- blackjack.py
+++ blackjack.py
@@ -2,7 +2,7 @@ import gym
 from collections import defaultdict
 
 # init gym environment
-env = gym.make('Blackjack-v0')
+env = gym.make('Blackjack-v1')
 
 
 # define policy to hit until 19 (0=stand, 1=hit)
@@ -29,11 +29,12 @@ def generate_episode_data(policy, env):
         # perform action in environment
         observation, reward, done, info = env.step(action)
         # add the reward to rewards list
+        # print(reward)
         rewards.append(reward)
         # break if state is terminal
         if done:
             break
-        return states, actions, rewards
+    return states, actions, rewards
 
 
 # define first-visit mc prediction function
@@ -42,35 +43,34 @@ def fv_mc_prediction(policy, env, n_episodes):
     value_table = defaultdict(float)
     N = defaultdict(int)
 
+
     # for each episode generate data and store
     for _ in range(n_episodes):
         # generate & store data
-        states, _, rewards = generate_episode_data(policy, env)
-        returns = 0;
+        data = generate_episode_data(policy, env)
+        returns = 0
+        if data is not None:
+            # for each step store rewards and states to temp variable and calculate returns as sum of rewards
+            # for each episode calculate current value of all the states involved (starting from terminal state)
+            for i in range(len(data[0]) - 1, -1, -1):
+                _reward = data[2][i]
+                _state = data[0][i]
 
-        # for each step store rewards and states to temp variable and calculate returns as sum of rewards
-        # for each episode calculate current value of all the states involved (starting from terminal state)
-
-        for i in range(len(states) -1, -1, -1):
-            _reward = rewards[i]
-            _state = states[i]
-
-            # add reward to returns
-            returns += _reward
-
-            # for first-visit check if episode is visited for the first time
-            if _state not in states[:i]:
-                # if yes standard mc incremental equation
-                # NewEstimate = OldEstimate + StepSize(Target-OldEstimate)
-                N[_state] += 1
-                value_table[_state] += (returns - value_table[_state]) / N[_state]
-
+                # add reward to returns
+                returns += _reward
+                # for first-visit check if episode is visited for the first time
+                if _state not in data[0][:i]:
+                    # if yes standard mc incremental equation
+                    # NewEstimate = OldEstimate + StepSize(Target-OldEstimate)
+                    N[_state] += 1
+                    value_table[_state] += (returns - value_table[_state]) / N[_state]
+            # print(returns)
     return value_table
 
 
+prediction = fv_mc_prediction(hit_til_19, env, n_episodes=500000)
 
 
-
-
-
+for j in range(10):
+    print(prediction.popitem())
 
