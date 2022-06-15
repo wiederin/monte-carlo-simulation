7817f2f4692d8a60fc87580562ea78e4e00dc234
wiederin <wiederin@usc.edu>
2022-06-15 13:17:21 +0200

    init plot_blackjack function

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,7 @@
 import gym
 from collections import defaultdict
+import numpy as np
+from matplotlib import pyplot
 
 # init gym environment
 env = gym.make('Blackjack-v1')
@@ -43,7 +45,6 @@ def fv_mc_prediction(policy, env, n_episodes):
     value_table = defaultdict(float)
     N = defaultdict(int)
 
-
     # for each episode generate data and store
     for _ in range(n_episodes):
         # generate & store data
@@ -68,9 +69,42 @@ def fv_mc_prediction(policy, env, n_episodes):
     return value_table
 
 
+# define function to plot blackjack results
+def plot_blackjack(p, ax1, ax2):
+    player_sum = np.arange(12, 21 + 1)
+    dealer_sum = np.arange(1, 10 + 1)
+    usable_ace = np.array([False, True])
+    state_values = np.zeros((len(player_sum), len(dealer_sum), len(usable_ace)))
+    # loop through player_sum values
+    for i, player in enumerate(player_sum):
+        # loop through dealer_sum values
+        for j, dealer in enumerate(dealer_sum):
+            # loop through usable_ace values
+            for k, ace in enumerate(usable_ace):
+                # create state_value
+                state_values[i, j, k] = p[player, dealer, ace]
+
+    # X, Y coordinates from player and dealer sums
+    X, Y = np.meshgrid(player_sum, dealer_sum)
+    ax1.plot_wireframe(X, Y, state_values[:, :, 0])
+    ax2.plot_wireframe(X, Y, state_values[:, :, 1])
+
+    for ax in ax1, ax2:
+        ax.set_zlim(-1, 1)
+        ax.set_ylabel('player_sum')
+        ax.set_xlabel('dealer_sum')
+        ax.set_zlabel('state_value')
+
+
 prediction = fv_mc_prediction(hit_til_19, env, n_episodes=500000)
 
-
+'''
 for j in range(10):
     print(prediction.popitem())
+'''        
 
+fig, axes = pyplot.subplots(nrows=2, figsize=(5, 8), subplot_kw={'projection': '3d'})
+
+axes[0].set_title('state-value distribution w/o usable ace')
+axes[1].set_title('state-value distribution w usable ace')
+plot_blackjack(prediction, axes[0], axes[1])
